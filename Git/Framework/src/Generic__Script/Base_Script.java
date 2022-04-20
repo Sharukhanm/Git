3808a04c26ca46f1b530f07bb45cd900b0161683
sharu <sharu@Sharukhan>
2022-04-20 15:30:19 +0530

    Second commit

---
+++ Git/Framework/src/Generic__Script/Base_Script.java
@@ -0,0 +1,32 @@
+package Generic__Script;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base_Script implements Framework_contains{
+	public static WebDriver driver;
+	@BeforeMethod
+	public void openapp()
+	{
+		System.setProperty(GECKO_KEY, GECKO_VALUE);
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get(URL); 
+		}
+	@AfterMethod
+	public void closeapp(ITestResult res) throws IOException
+	{  
+		if(ITestResult.SUCCESS==res.getStatus())
+		{
+		Generic_TakeScreenshot.getData(driver);	
+		}
+		driver.close();
+	}
+
+}
